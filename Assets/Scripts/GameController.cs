8596db6637fdcca0f905cb9c995c7bb38cfc6024
Nostale97 <87812522+DaniMa97@users.noreply.github.com>
2025-05-04 21:18:14 +0200

    First Iteration
    FPS template, basic player movement, imported models

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    static GameController instance;
+
+    bool isMaskOn = false;
+
+    public static GameController GetInstance()
+    {
+        return instance;
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void ChangeMask()
+    {
+        isMaskOn = !isMaskOn;
+    }
+
+    public bool GetIsMaskOn()
+    {
+        return isMaskOn;
+    }
+}
